2c6e1af7a4008e71cb6306b97ee5116a39f67a4f
habibrodriguez7-art <habibrodriguez7@gmail.com>
2026-06-11 08:06:58 +0800

    coord: add Sisyphus Statue

--- Coord.lua
+++ Coord.lua
@@ -1,5 +1,6 @@
 local Coords = {
 	["Copper Canyon Mines"] = { pos = {-4079.106, -547.174, 548.035}, look = {-4070.006, -547.174, 543.889} },
+	["Sisyphus Statue"] = { pos = {-3656.654, -134.358, -963.251}, look = {-3666.39, -134.358, -960.967} },
 }
 
 return Coords
